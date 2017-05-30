78b1f12d57c98435c26091fc03517fb208cd1e13
Ethan Mallove <mallove@fas.harvard.edu>
2017-05-30 09:38:47 -0400

    #9441 - more deployment tests

--- openscholar/themes/os_basetheme/templates/page.tpl.php
+++ openscholar/themes/os_basetheme/templates/page.tpl.php
@@ -1,4 +1,7 @@
 Tue May 30 09:04:34 EDT 2017
+<?php print "<br>'" . variable_get('google_credentials') . "'"; ?>
+<?php print "<br>'" . variable_get('google_client_secret') . "'"; ?>
+
 <div id="page_wrap" class="page_wrap">
 <!--REGION TO HOUSE RESPONSIVE MENU. OTHER CONTENT CAN'T BE PLACED HERE-->
 <div class="responive-menu-container clearfix">
